d417b719aaea3ee2bc5bfa87623e47eee4a465ff
s1mple <798342418@qq.com>
2018-11-27 15:17:04 +0800

    add should_merge_new_entity_with_different_merge_level test

--- src/test/java/xyz/s1mple/merging/MergingTest.java
+++ src/test/java/xyz/s1mple/merging/MergingTest.java
@@ -5,9 +5,34 @@ import xyz.s1mple.merging.exceptions.MergeWithDifferentClassTypeException;
 import xyz.s1mple.merging.model.Entity;
 import xyz.s1mple.merging.model.NoFieldsEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MergingTest {
     @Test(expected = MergeWithDifferentClassTypeException.class)
     public void should_throw_MergingException_when_merge_with_different_class_type() {
         new Entity().mergeWith(new NoFieldsEntity());
+    }
+
+    @Test
+    public void should_merge_new_entity_with_different_merge_level() {
+        Entity newEntity = new Entity();
+        newEntity.setString("newString");
+        newEntity.setAnInt(2);
+        newEntity.setStringList(null);
+        newEntity.setStrings(new String[]{"newS1", "newS2", "newS3"});
+        newEntity.setEmail(new Entity.Email("newEmail@163.com"));
+        newEntity.setEmails(null);
+
+        Entity entity = new Entity();
+        entity.mergeWith(newEntity);
+
+        assertThat(entity.getString()).isEqualTo("string");
+        assertThat(entity.getAnInt()).isEqualTo(2);
+        assertThat(entity.getStringList()).isNull();
+        assertThat(entity.getStrings()).containsSequence("newS1", "newS2", "newS3");
+        assertThat(entity.getEmail().getValue()).isEqualTo("newEmail@163.com");
+        assertThat(entity.getEmails()).hasSize(2);
+        assertThat(entity.getEmails()).contains(new Entity.Email("zz@163.com"));
+        assertThat(entity.getEmails()).contains(new Entity.Email("zz@gmail.com"));
     }
 }
--- src/test/java/xyz/s1mple/merging/model/Entity.java
+++ src/test/java/xyz/s1mple/merging/model/Entity.java
@@ -2,17 +2,34 @@ package xyz.s1mple.merging.model;
 
 import lombok.Data;
 import lombok.ToString;
+import org.assertj.core.util.Sets;
+import xyz.s1mple.merging.Level;
 import xyz.s1mple.merging.MergeOn;
 import xyz.s1mple.merging.Merging;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
-@Data
 @ToString
+@Data
 public class Entity implements Merging {
-    @MergeOn
-    private String string;
-    private int anInt;
-    private List<String> stringList;
-    private String[] strings;
+    @MergeOn(level = Level.Ignored)
+    private String string = "string";
+    private int anInt = 1;
+    @MergeOn(level = Level.Mandatory)
+    private List<String> stringList = Arrays.asList("a", "b", "c");
+    @MergeOn(level = Level.Required)
+    private String[] strings = new String[]{"sArr1", "sArr2"};
+    private Email email = new Email("zz@163.com");
+    private Set<Email> emails = Sets.newLinkedHashSet(new Email("zz@163.com"), new Email("zz@gmail.com"));
+
+    @Data
+    public static class Email {
+        private String value;
+
+        public Email(String value) {
+            this.value = value;
+        }
+    }
 }
