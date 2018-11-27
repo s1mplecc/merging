3e443702ab535db1962e521402866755cb30c240
s1mple <798342418@qq.com>
2018-11-27 13:19:30 +0800

    merge with new entity by @MergeOn annotation level

--- src/main/java/xyz/s1mple/merging/Level.java
+++ src/main/java/xyz/s1mple/merging/Level.java
@@ -1,12 +1,12 @@
 package xyz.s1mple.merging;
 
 public enum Level {
-    /* 无论新值为何止，都会覆盖旧值 */
+    /* 无论新值为何值，都会覆盖旧值 */
     Mandatory,
     /* 如果新值不为null，覆盖旧值，否则不覆盖 */
     Required,
     /* 忽略，不做合并处理 */
-    Ignore;
+    Ignored;
 
     public boolean isRequired() {
         return this.equals(Required);
@@ -16,7 +16,7 @@ public enum Level {
         return this.equals(Mandatory);
     }
 
-    public boolean isIgnore() {
-        return this.equals(Ignore);
+    public boolean isIgnored() {
+        return this.equals(Ignored);
     }
 }
--- src/main/java/xyz/s1mple/merging/Merging.java
+++ src/main/java/xyz/s1mple/merging/Merging.java
@@ -9,33 +9,37 @@ import java.lang.reflect.Field;
 public interface Merging {
     Logger log = LoggerFactory.getLogger(Merging.class);
 
-    default <U> void mergeWith(U newModel) {
-        if (this.getClass() != newModel.getClass()) {
+    default <T extends Merging> void mergeWith(T newEntity) {
+        if (this.getClass() != newEntity.getClass()) {
             throw new MergeWithDifferentClassTypeException(
-                    String.format("<%s> can not merge with other class type <%s>", this.getClass().getName(), newModel.getClass().getName()));
+                    String.format("<%s> can not merge with other class type <%s>", this.getClass().getName(), newEntity.getClass().getName()));
         }
 
-        Field[] oldFields = this.getClass().getDeclaredFields();
-        Field[] newFields = newModel.getClass().getDeclaredFields();
+        Field[] fields = this.getClass().getDeclaredFields();
 
-        if (oldFields.length == 0) {
+        if (fields.length == 0) {
             log.warn("class <{}> has no declared fields", this.getClass().getName());
             return;
         }
 
-        for (int i = 0; i < oldFields.length; i++) {
-            Field oldField = oldFields[i];
-            Field newField = newFields[i];
-            oldField.setAccessible(true);
-            newField.setAccessible(true);
+        for (Field field : fields) {
+            MergeOn mergeOn = field.getAnnotation(MergeOn.class);
+            field.setAccessible(true);
 
             try {
-                oldField.set(this, newField.get(newModel));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+                if (mergeOn == null || mergeOn.level().isRequired()) {
+                    Object newFieldValue = field.get(newEntity);
+                    if (newFieldValue != null) {
+                        field.set(this, newFieldValue);
+                    }
+                    continue;
+                }
+                if (mergeOn.level().isMandatory()) {
+                    field.set(this, field.get(newEntity));
+                }
+            } catch (IllegalAccessException ex) {
+                log.error(ex.getMessage());
             }
         }
-
-        System.out.println(this);
     }
 }
